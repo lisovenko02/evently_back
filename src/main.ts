d6aae721b76de8cd4d3c59a13f0ed2f3da316cbc
lisovenko02 <vanyalisovenkoo@gmail.com>
2026-03-02 19:29:35 +0200

    update origin frontend url

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://evently-front-taupe.vercel.app/',
     methods: 'GET,POST,PATCH,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
